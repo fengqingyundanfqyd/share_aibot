8488f44768a679de521d30471d9d41364f52d60b
xiongyunfei <xiongyf0711@163.com>
2017-09-27 11:21:35 +0800

    我的地址  修改侧滑删除和下拉刷新

--- aibot/app/src/main/java/com/example/aiqing/sharerobot/activity/MyAddressActivity.java
+++ aibot/app/src/main/java/com/example/aiqing/sharerobot/activity/MyAddressActivity.java
@@ -4,13 +4,16 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,6 +51,9 @@ public class MyAddressActivity extends AppCompatActivity {
     private String mDistrict;
     private Dialog mLoadingDialog;
     private String mAddressId;
+    private ListView mLvMyaddress;
+    private SwipeRefreshLayout mRefresh;
+    private MyAddressAdapter mMyAddressAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -73,8 +79,11 @@ public class MyAddressActivity extends AppCompatActivity {
 
     private void initId() {
         TextView tvAdd = (TextView) findViewById(R.id.tv_add_myaddress);
+        mLvMyaddress = (ListView) findViewById(R.id.lv_myaddress);
+        mRefresh = (SwipeRefreshLayout) findViewById(R.id.refresh);
 
-        mSidelv = (SideslipListView) findViewById(R.id.sideliplistview);
+
+        // mSidelv = (SideslipListView) findViewById(R.id.sideliplistview);
 
         tvAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,72 +96,90 @@ public class MyAddressActivity extends AppCompatActivity {
             }
         });
 
+        mRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                handler.sendEmptyMessageDelayed(199,1000);
+            }
+        });
+
         //条目点击监听
-        mSidelv.setOnItemClickListener(new AddressItemClickListener());
+//        mSidelv.setOnItemClickListener(new AddressItemClickListener());
     }
-
-    class AddressItemClickListener implements AdapterView.OnItemClickListener {
-
+    private Handler handler=new Handler(){
         @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            //mLoadingDialog = DialogUtil.createLoadingDialog(MyAddressActivity.this, "加载中...");
-            mAddressId = mResult.get(position).getAddressId();
-            Log.e("mAddressId", "onItemClick: "+ mAddressId);
-            String name = mResult.get(position).getName();
-            String mobile = mResult.get(position).getMobile();
-            mProvince = mResult.get(position).getProvince();
-            mCity = mResult.get(position).getCity();
-            mDistrict = mResult.get(position).getDistrict();
-            String township = mResult.get(position).getTownship();
-            String street = mResult.get(position).getStreet();
-            String streetnumber = mResult.get(position).getStreetnumber();
-            String building = mResult.get(position).getBuilding();
-            String isDefault = mResult.get(position).getIsDefault();
-            String fAddress = mResult.get(position).getFAddress();
-            String dAddress = mResult.get(position).getDAddress();
-            String lgt = mResult.get(position).getLgt();
-            String lat = mResult.get(position).getLat();
-
-            String add=mProvince+mCity+mDistrict;
-
-            Intent intent = new Intent();
-            intent.putExtra("mAddressId",mAddressId);
-            intent.putExtra("name",name);
-            intent.putExtra("mobile",mobile);
-            intent.putExtra("add",add);
-
-            setResult(4,intent);
-            finish();
-
-            Log.e("地址id", "onItemClick: "+ mAddressId);
-
-//            Toast.makeText(MyAddressActivity.this, "点击了" + lgt + mDistrict + building, Toast.LENGTH_SHORT).show();
-//            Retrofit builder = new Retrofit.Builder()
-//                    .client(mHttpTool.client())
-//                    .baseUrl("https://shared.aqcome.com/account/updateAddress.shtml")
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .build();
-//            ApiService apiService = builder.create(ApiService.class);
-//
-//            Call<UpdateAddressBean> call = apiService.updateadd(mSid, mAddressId, name, mobile, mProvince, mCity, mDistrict, township, street
-//                    , streetnumber, building, isDefault, fAddress, dAddress, lgt, lat);
-//            call.enqueue(new Callback<UpdateAddressBean>() {
-//                @Override
-//                public void onResponse(Response<UpdateAddressBean> response, Retrofit retrofit) {
-//                    Log.e("修改地址", "onResponse: " + response.body().isSuccess());
-////                    Intent intent = new Intent();
-////                    intent.setClass(MyAddressActivity.this, AddAddressActivity.class);
-////                    startActivity(intent);
-//                }
-//
-//                @Override
-//                public void onFailure(Throwable t) {
-//                    Toast.makeText(MyAddressActivity.this, "网络连接失败，请检查您的网络！", Toast.LENGTH_SHORT).show();
-//                    Log.e("666", "onFailure: " + "网络连接失败");
-//                }
-//            });
+        public void handleMessage(Message msg) {
+            if(msg.what==199){
+                    if (mMyAddressAdapter!=null){
+                        mMyAddressAdapter.notifyDataSetChanged();
+                    }
+                //设置组件的刷洗状态；false代表关闭
+                mRefresh.setRefreshing(false);
+            }
         }
-    }
+    };
+//    class AddressItemClickListener implements AdapterView.OnItemClickListener {
+//
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            //mLoadingDialog = DialogUtil.createLoadingDialog(MyAddressActivity.this, "加载中...");
+//            mAddressId = mResult.get(position).getAddressId();
+//            Log.e("mAddressId", "onItemClick: "+ mAddressId);
+//            String name = mResult.get(position).getName();
+//            String mobile = mResult.get(position).getMobile();
+//            mProvince = mResult.get(position).getProvince();
+//            mCity = mResult.get(position).getCity();
+//            mDistrict = mResult.get(position).getDistrict();
+//            String township = mResult.get(position).getTownship();
+//            String street = mResult.get(position).getStreet();
+//            String streetnumber = mResult.get(position).getStreetnumber();
+//            String building = mResult.get(position).getBuilding();
+//            String isDefault = mResult.get(position).getIsDefault();
+//            String fAddress = mResult.get(position).getFAddress();
+//            String dAddress = mResult.get(position).getDAddress();
+//            String lgt = mResult.get(position).getLgt();
+//            String lat = mResult.get(position).getLat();
+//
+//            String add=mProvince+mCity+mDistrict;
+//
+//            Intent intent = new Intent();
+//            intent.putExtra("mAddressId",mAddressId);
+//            intent.putExtra("name",name);
+//            intent.putExtra("mobile",mobile);
+//            intent.putExtra("add",add);
+//
+//            setResult(4,intent);
+//            finish();
+//
+//            Log.e("地址id", "onItemClick: "+ mAddressId);
+//
+////            Toast.makeText(MyAddressActivity.this, "点击了" + lgt + mDistrict + building, Toast.LENGTH_SHORT).show();
+////            Retrofit builder = new Retrofit.Builder()
+////                    .client(mHttpTool.client())
+////                    .baseUrl("https://shared.aqcome.com/account/updateAddress.shtml")
+////                    .addConverterFactory(GsonConverterFactory.create())
+////                    .build();
+////            ApiService apiService = builder.create(ApiService.class);
+////
+////            Call<UpdateAddressBean> call = apiService.updateadd(mSid, mAddressId, name, mobile, mProvince, mCity, mDistrict, township, street
+////                    , streetnumber, building, isDefault, fAddress, dAddress, lgt, lat);
+////            call.enqueue(new Callback<UpdateAddressBean>() {
+////                @Override
+////                public void onResponse(Response<UpdateAddressBean> response, Retrofit retrofit) {
+////                    Log.e("修改地址", "onResponse: " + response.body().isSuccess());
+//////                    Intent intent = new Intent();
+//////                    intent.setClass(MyAddressActivity.this, AddAddressActivity.class);
+//////                    startActivity(intent);
+////                }
+////
+////                @Override
+////                public void onFailure(Throwable t) {
+////                    Toast.makeText(MyAddressActivity.this, "网络连接失败，请检查您的网络！", Toast.LENGTH_SHORT).show();
+////                    Log.e("666", "onFailure: " + "网络连接失败");
+////                }
+////            });
+//        }
+//    }
     private void initHttp() {
         mLoadingDialog = DialogUtil.createLoadingDialog(this, "加载中...");
         Retrofit builder = new Retrofit.Builder()
@@ -169,8 +196,9 @@ public class MyAddressActivity extends AppCompatActivity {
                 DialogUtil.closeDialog(mLoadingDialog);
                 if (response.body()!=null) {
                     mResult = response.body().getObj().getResult();
-                    MyAddressAdapter myAddressAdapter = new MyAddressAdapter();
-                    mSidelv.setAdapter(myAddressAdapter);
+                    mMyAddressAdapter = new MyAddressAdapter();
+//                    mSidelv.setAdapter(myAddressAdapter);
+                    mLvMyaddress.setAdapter(mMyAddressAdapter);
                 }
             }
             @Override
@@ -184,7 +212,7 @@ public class MyAddressActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 5;
+            return mResult.size();
         }
 
         @Override
@@ -198,11 +226,12 @@ public class MyAddressActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, android.view.View convertView, ViewGroup parent) {
+        public View getView(final int position, android.view.View convertView, ViewGroup parent) {
             ViewHolder viewHolder;
             if (convertView == null) {
                 viewHolder = new ViewHolder();
                 convertView = View.inflate(MyAddressActivity.this, R.layout.item_myaddress, null);
+               // SwipeLayout swipelayout = (SwipeLayout) convertView.findViewById(R.id.swipe);
                 viewHolder.tvName = (TextView) convertView.findViewById(R.id.tv_addname);
                 viewHolder.tvPhonenum = (TextView) convertView.findViewById(R.id.tv_addnum);
                 viewHolder.tvMorenAddress = (TextView) convertView.findViewById(R.id.tv_addaddress);
@@ -242,6 +271,8 @@ public class MyAddressActivity extends AppCompatActivity {
                     call.enqueue(new Callback<DeleteAddressBean>() {
                         @Override
                         public void onResponse(Response<DeleteAddressBean> response, Retrofit retrofit) {
+
+
                             boolean success = response.body().isSuccess();
                             Log.e("删除地址", "onResponse: "+success );
                             Toast.makeText(MyAddressActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
@@ -254,7 +285,7 @@ public class MyAddressActivity extends AppCompatActivity {
                     });
                     mResult.remove(pos);
                     notifyDataSetChanged();
-                    mSidelv.turnNormal();
+                 //   mSidelv.turnNormal();
                 }
 
             });
